749329d271ce6a1110262258d260fb6ec77ab136
Sebastian Tromer <tromersebastian@gmail.com>
2018-09-08 00:05:43 +0200

    sam3 i2c implemented read and write functions. Not tested on hw

--- cpu/sam3/periph/i2c.c
+++ cpu/sam3/periph/i2c.c
@@ -19,8 +19,12 @@
  * @}
  */
 
+#include <stdint.h>
+#include <errno.h>
+
 #include "cpu.h"
 #include "mutex.h"
+
 #include "assert.h"
 #include "periph/gpio.h"
 #include "periph/i2c.h"
@@ -36,6 +40,8 @@
 #define TWI_CLK_DIV_MAX      0xFF
 #define TWI_CLK_DIV_MIN      7
 
+/** Time-out value (number of attempts). */
+#define TWI_TIMEOUT 15000
 
 /**
  * @brief   Array holding one pre-initialized mutex for each I2C device
@@ -62,6 +68,30 @@ static inline void twi_enable_master_mode(Twi *p_twi)
 
     /* Set Master Enable bit */
     p_twi->TWI_CR = TWI_CR_MSEN;
+}
+
+/**
+ * \internal
+ * \brief Construct the TWI module address register field
+ *
+ * The TWI module address register is sent out MSB first. And the size controls
+ * which byte is the MSB to start with.
+ *
+ * Please see the device datasheet for details on this.
+ */
+static uint32_t twi_mk_addr(const uint16_t reg, int len)
+{
+    uint32_t val;
+
+    if (len == 0)
+        return 0;
+
+    val = reg & 0xFF;
+    if (len > 1) {
+        val <<= 8;
+        val |= (reg & 0xFF00);
+    }
+    return val;
 }
 
 static void i2c_set_speed(i2c_t dev, uint32_t ul_speed)
@@ -176,32 +206,214 @@ int i2c_release(i2c_t dev)
 int i2c_read_regs(i2c_t dev, uint16_t addr, uint16_t reg,
         void *data, size_t len, uint8_t flags)
 {
-    (void) dev;
-    (void) addr; (void) reg; (void) data; (void) len; (void) flags;
-    return -1;
+    uint32_t status;
+    uint8_t reg_len = (flags | I2C_REG16) ? 2 : 1;
+    uint32_t cnt = len;
+    uint8_t *buffer = data;
+    uint8_t stop_sent = 0;
+    uint32_t timeout = TWI_TIMEOUT;
+
+    /* Check argument */
+    if (cnt == 0)
+        return -EINVAL;
+
+    /* Set read mode, slave address and 3 internal address byte lengths */
+    twi(dev)->TWI_MMR = 0;
+    twi(dev)->TWI_MMR = TWI_MMR_MREAD | TWI_MMR_DADR(addr) |
+        ((reg_len << TWI_MMR_IADRSZ_Pos) &
+         TWI_MMR_IADRSZ_Msk);
+
+    /* Set internal address for remote chip */
+    twi(dev)->TWI_IADR = 0;
+    twi(dev)->TWI_IADR = twi_mk_addr(reg, reg_len);
+
+    /* Send a START condition */
+    if (cnt == 1) {
+        twi(dev)->TWI_CR = TWI_CR_START | TWI_CR_STOP;
+        stop_sent = 1;
+    } else {
+        twi(dev)->TWI_CR = TWI_CR_START;
+        stop_sent = 0;
+    }
+
+    while (cnt > 0) {
+        status = twi(dev)->TWI_SR;
+        if (status & TWI_SR_NACK)
+            return -EIO;
+
+        if (!timeout--)
+            return -ETIMEDOUT;
+
+        /* Last byte ? */
+        if (cnt == 1  && !stop_sent) {
+            twi(dev)->TWI_CR = TWI_CR_STOP;
+            stop_sent = 1;
+        }
+
+        if (!(status & TWI_SR_RXRDY))
+            continue;
+
+        *buffer++ = twi(dev)->TWI_RHR;
+
+        cnt--;
+        timeout = TWI_TIMEOUT;
+    }
+
+    while (!(twi(dev)->TWI_SR & TWI_SR_TXCOMP));
+    twi(dev)->TWI_SR;
+
+    return 0;
 }
 
 int i2c_read_bytes(i2c_t dev, uint16_t addr,
         void *data, size_t len, uint8_t flags)
 {
-    (void) dev;
-    (void) addr; (void) data; (void) len; (void) flags;
-    return -1;
+    (void) flags;
+
+    uint32_t status;
+    uint32_t cnt = len;
+    uint8_t *buffer = data;
+    uint8_t stop_sent = 0;
+    uint32_t timeout = TWI_TIMEOUT;
+
+    /* Check argument */
+    if (cnt == 0)
+        return -EINVAL;
+
+    /* Set read mode and slave address */
+    twi(dev)->TWI_MMR = 0;
+    twi(dev)->TWI_MMR = TWI_MMR_MREAD | TWI_MMR_DADR(addr);
+
+    /* Send a START condition */
+    if (cnt == 1) {
+        twi(dev)->TWI_CR = TWI_CR_START | TWI_CR_STOP;
+        stop_sent = 1;
+    } else {
+        twi(dev)->TWI_CR = TWI_CR_START;
+        stop_sent = 0;
+    }
+
+    while (cnt > 0) {
+        status = twi(dev)->TWI_SR;
+        if (status & TWI_SR_NACK)
+            return -EIO;
+
+        if (!timeout--)
+            return -ETIMEDOUT;
+
+        /* Last byte ? */
+        if (cnt == 1  && !stop_sent) {
+            twi(dev)->TWI_CR = TWI_CR_STOP;
+            stop_sent = 1;
+        }
+
+        if (!(status & TWI_SR_RXRDY))
+            continue;
+
+        *buffer++ = twi(dev)->TWI_RHR;
+
+        cnt--;
+        timeout = TWI_TIMEOUT;
+    }
+
+    while (!(twi(dev)->TWI_SR & TWI_SR_TXCOMP));
+    twi(dev)->TWI_SR;
+
+    return 0;
 }
 
 int i2c_write_bytes(i2c_t dev, uint16_t addr, const void *data,
         size_t len, uint8_t flags)
 {
-    (void) dev;
-    (void) addr; (void) data; (void) len; (void) flags;
-    return -1;
+    uint32_t status;
+    uint32_t cnt = len;
+    const uint8_t *buffer = data;
+
+    /* Check argument */
+    if (cnt == 0)
+        return -EINVAL;
+
+    /* Set write mode and slave address */
+    twi(dev)->TWI_MMR = 0;
+    twi(dev)->TWI_MMR = TWI_MMR_DADR(addr);
+
+    /* Send all bytes */
+    while (cnt > 0) {
+        status = twi(dev)->TWI_SR;
+        if (status & TWI_SR_NACK)
+            return -EIO;
+        if (!(status & TWI_SR_TXRDY))
+            continue;
+
+        twi(dev)->TWI_THR = *buffer++;
+        cnt--;
+    }
+
+    while (1) {
+        status = twi(dev)->TWI_SR;
+        if (status & TWI_SR_NACK)
+            return -EIO;
+
+        if (status & TWI_SR_TXRDY)
+            break;
+    }
+
+    if (!(flags | I2C_NOSTOP)) {
+        /* Send stop */
+        twi(dev)->TWI_CR = TWI_CR_STOP;
+        while (!(twi(dev)->TWI_SR & TWI_SR_TXCOMP));
+    }
+
+    return 0;
 }
 
 int i2c_write_regs(i2c_t dev, uint16_t addr, uint16_t reg,
         const void *data, size_t len, uint8_t flags)
 {
-    (void) dev;
-    (void) addr; (void) reg; (void) data; (void) len; (void) flags;
-    return -1;
+    uint32_t status;
+    uint8_t reg_len = (flags | I2C_REG16) ? 2 : 1;
+    uint32_t cnt = len;
+    const uint8_t *buffer = data;
+
+    /* Check argument */
+    if (cnt == 0)
+        return -EINVAL;
+
+    /* Set write mode, slave address and 3 internal address byte lengths */
+    twi(dev)->TWI_MMR = 0;
+    twi(dev)->TWI_MMR = TWI_MMR_DADR(addr) |
+        ((reg_len << TWI_MMR_IADRSZ_Pos) &
+         TWI_MMR_IADRSZ_Msk);
+
+    /* Set internal address for remote chip */
+    twi(dev)->TWI_IADR = 0;
+    twi(dev)->TWI_IADR = twi_mk_addr(reg, reg_len);
+
+    /* Send all bytes */
+    while (cnt > 0) {
+        status = twi(dev)->TWI_SR;
+        if (status & TWI_SR_NACK)
+            return -EIO;
+        if (!(status & TWI_SR_TXRDY))
+            continue;
+
+        twi(dev)->TWI_THR = *buffer++;
+        cnt--;
+    }
+
+    while (1) {
+        status = twi(dev)->TWI_SR;
+        if (status & TWI_SR_NACK)
+            return -EIO;
+
+        if (status & TWI_SR_TXRDY)
+            break;
+    }
+
+    /* Send stop */
+    twi(dev)->TWI_CR = TWI_CR_STOP;
+    while (!(twi(dev)->TWI_SR & TWI_SR_TXCOMP));
+
+    return 0;
 }
 
